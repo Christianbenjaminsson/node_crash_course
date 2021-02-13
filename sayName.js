848968a87033a4d714d839b909ded84b9802fb51
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-02-13 12:06:53 +0100

    Initial commit

---
+++ sayName.js
@@ -0,0 +1,10 @@
+const sayName = () => {
+  console.log('Hello there Ed');
+};
+
+const sayAdress = () => {
+  console.log('Jhonson Street');
+};
+
+module.exports.sayName = sayName;
+module.exports.sayAdress = sayAdress;
