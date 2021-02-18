ae84b5ce571d29d4444e9932542dfb719e60d907
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-02-18 14:34:09 +0100

    Create http.js and create server

---
+++ http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.write('Hello there user');
+  res.end();
+});
+
+server.listen(3000, () => console.log('Server is up and running'));
