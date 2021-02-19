b10df78a25258d32e71977a3978bf7cd4046189c
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-02-19 17:41:39 +0100

    Continue working with http

--- http.js
+++ http.js
@@ -1,9 +1,21 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
   console.log(req);
   res.write('Hello there user');
   res.end();
+});*/
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html' })
+    res.write('<h1>Welcome to the homepage</h1>');
+    res.end();
+  }
+  if (req.url === '/user') {
+    res.write('Welcome user Jhonson');
+    res.end();
+  }
 });
 
 server.listen(3000, () => console.log('Server is up and running'));
