68f61c96735586bf554b32b37bf60085ab65ced5
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-02-20 17:18:09 +0100

    Continue with http

--- http.js
+++ http.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const path = rewuire('path');
+const fs = require('fs');
 
 /*const server = http.createServer((req, res) => {
   console.log(req);
@@ -6,11 +8,27 @@ const http = require('http');
   res.end();
 });*/
 
+/*const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html' });
+    res.write('<h1>Welcome to the homepage</h1>');
+    res.write('<h1>Welcome to the homepage</h1>');
+    res.write('<p>Additionalcontent</p>');
+    res.end();
+  }
+  if (req.url === '/user') {
+    res.write('Welcome user Jhonson');
+    res.end();
+  }
+});*/
+
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html' })
-    res.write('<h1>Welcome to the homepage</h1>');
-    res.end();
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    });
   }
   if (req.url === '/user') {
     res.write('Welcome user Jhonson');
